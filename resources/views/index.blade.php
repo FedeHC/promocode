91f8ed21fad6d1e46f27822dc16de0ea274c2657
Marco Sotomayor <marco.soto1995@gmail.com>
2018-08-03 16:34:12 -0300

    Agregado el Layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,21 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Probando</title>
-</head>
-<body>
-    <h1>Welcome
-        @guest
-            {{ "Stranger" }}
-        @else
-            {{ @Auth::user()->name }}
-        @endif !</h1>
-    <p>Acá tiene que ir todo el blade de bienvenida y también la<br>pantalla principal cuando un user esté logueado.</p>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+        <!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Probando</title>
+        </head>
+        <body>
+            <h1>Welcome
+                @guest
+                    {{ "Stranger" }}
+                @else
+                    {{ @Auth::user()->name }}
+                @endif !</h1>
+            <p>Acá tiene que ir todo el blade de bienvenida y también la<br>pantalla principal cuando un user esté logueado.</p>
+        </body>
+        </html>
+@endsection
