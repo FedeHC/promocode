f8178f17610d3b614de04030037851c490a05b1c
Marco Sotomayor <marco.soto1995@gmail.com>
2018-08-03 16:44:19 -0300

    Jumbotron

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-        <!DOCTYPE html>
-        <html lang="es">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>Probando</title>
-        </head>
-        <body>
-            <h1>Welcome
-                @guest
-                    {{ "Stranger" }}
-                @else
-                    {{ @Auth::user()->name }}
-                @endif !</h1>
-            <p>Acá tiene que ir todo el blade de bienvenida y también la<br>pantalla principal cuando un user esté logueado.</p>
-        </body>
-        </html>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Welcome
+            @guest
+                {{ "Stranger" }}
+            @else
+                {{ @Auth::user()->name }}
+            @endif !</h1>
+        </div>
+    </div>
+        {{--<!DOCTYPE html>--}}
+        {{--<html lang="es">--}}
+        {{--<head>--}}
+            {{--<meta charset="UTF-8">--}}
+            {{--<meta name="viewport" content="width=device-width, initial-scale=1">--}}
+            {{--<title>Probando</title>--}}
+        {{--</head>--}}
+        {{--<body>--}}
+            {{--<h1>Welcome--}}
+                {{--@guest--}}
+                    {{--{{ "Stranger" }}--}}
+                {{--@else--}}
+                    {{--{{ @Auth::user()->name }}--}}
+                {{--@endif !</h1>--}}
+        {{--</body>--}}
+        {{--</html>--}}
 @endsection
