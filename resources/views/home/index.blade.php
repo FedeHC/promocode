55e0093e4e0455d1dc07ade5b69616fd90cd085c
Marco Sotomayor <marco.soto1995@gmail.com>
2018-08-11 00:56:45 -0300

    probando

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,14 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-            <h1 class="display-4">Welcome
-            @guest
-                {{ "Stranger" }}
-            @else
-                {{ @Auth::user()->name }}
-            @endif !</h1>
-        </div>
-    </div>
+
 @endsection
