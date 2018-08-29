eca947639583c0fe9c374870a5d908ad0de9e267
FacuBlancoGofer <fmblanco97@gmail.com>
2018-08-28 23:11:27 -0300

    añadido html para boton Modify de la vista Products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -27,6 +27,7 @@
                                         <th scope="col">Value</th>
                                         <th scope="col">Detail</th>
                                         <th scope="col">Delete</th>
+                                        <th scope="col"> </th>
                                     </tr>
                                     </thead>
 
@@ -43,7 +44,46 @@
                                                     <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" name="checkbox[]" value="{{$fila->id}}" aria-label="...">
                                                 </div>
                                             </td>
+                                            <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal_Modify">
+                                            Modify
+                                            </button></td>
                                         </tr>
+
+                                        <!-- Modal Modify -->
+                <div class="modal fade" id="modal_Modify" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLongTitle"></h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <form method="post" id="mod_form" action= "{{ route('products.update') }}">
+                                @csrf
+                                <div class="modal-body">
+
+                                    <div class="form-group">
+                                        <label for="add_product">Product name</label>
+                                        <input type="text" class="form-control" id="mod_product" value= "{{ $fila->name }}"  name="product_name" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="add_value">Product value</label>
+                                        <input type="number" class="form-control" id="mod_value" value= "{{ $fila->value }}" name="product_value" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="add_detail">Product detail</label>
+                                        <input type="text" class="form-control" id="mod_detail" value= "{{ $fila->detail }}" name="product_detail" required>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Confirm</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
                                     @endforeach
                                     </tbody>
                                 </table>
@@ -57,13 +97,13 @@
                 <br><br>
 
                 <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal_Add">
                     Add
                 </button>
                 <button type="submit" class="btn btn-primary" form="delete_form">Delete</button>
 
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <!-- Modal Add -->
+                <div class="modal fade" id="modal_Add" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
